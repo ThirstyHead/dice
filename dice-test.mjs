20ce71416dfc6b868d129b67627eb727bb58e67b
Scott Davis <scott@thirstyhead.com>
2018-07-09 17:16:18 -0600

    added dice.roll()

--- dice-test.mjs
+++ dice-test.mjs
@@ -12,6 +12,17 @@ tests.set("it should allow you to set the number of sides", () => {
   return d.sides === 10;
 });
 
+tests.set("roll() should be between 1 and dice.sides", () => {
+  const d = new Dice();
+  let tmp = [];
+  for(let i=0; i<100; i++) {
+    tmp.push(d.roll());
+  }
+  return tmp.every( (roll) => {
+    return roll >= 1 && roll <= d.sides;
+  });
+});
+
 let results = [];
 tests.forEach( (test, name) => {
   results.push(`[${test()}] ${name}`);
--- dice.mjs
+++ dice.mjs
@@ -4,4 +4,8 @@ export class Dice{
   constructor(sides=6){
     this.sides = sides;
   }
+
+  roll(){
+    return Math.floor(Math.random() * this.sides) + 1;
+  }
 }
